63ace26083530f8c52f2b42dc56cc66cbe687111
redjackfred <redjackfred@gmail.com>
2025-07-11 19:37:23 -0700

    move assets

--- src/QRCode.jsx
+++ src/QRCode.jsx
@@ -1,8 +1,12 @@
 export default function QRCodeComponent() {
 	return (
-		<div className="bg-white rounded-2xl p-4 w-fit text-center m-auto">
+		<div className="bg-white rounded-2xl p-4 w-fit text-center m-auto shadow-xl">
 			<div className="w-72 flex flex-col">
-				<img src="/images/image-qr-code.png" className="w-72 h-72 rounded-xl" />
+				<img
+					src="src/assets/images/image-qr-code.png"
+					className="w-72 h-72 rounded-xl"
+					alt="QR Code"
+				/>
 				<div className="px-4 my-6">
 					<h1 className="text-[22px] text-slate-900 font-bold mb-4 tracking-normal leading-[120%] font-display">
 						Improve your front-end skills by building projects
